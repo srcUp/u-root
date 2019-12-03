f151c5455ecc672f823977890ea6f5f06e61b590
Simran Singh <simran.p.singh@oracle.com>
2019-12-03 09:38:47 -0800

    securelaunch: Add launcher and eventlog sub pkgs.
    Signed-off-by: Simran Singh <simran.p.singh@oracle.com>

--- pkg/securelaunch/policy/policy.go
+++ pkg/securelaunch/policy/policy.go
@@ -8,6 +8,8 @@ import (
 	"github.com/u-root/u-root/pkg/diskboot"
 	"github.com/u-root/u-root/pkg/mount"
 	slaunch "github.com/u-root/u-root/pkg/securelaunch"
+	"github.com/u-root/u-root/pkg/securelaunch/eventlog"
+	"github.com/u-root/u-root/pkg/securelaunch/launcher"
 	"github.com/u-root/u-root/pkg/securelaunch/measurement"
 )
 
@@ -20,6 +22,8 @@ import (
 type policy struct {
 	DefaultAction string
 	Collectors    []measurement.Collector
+	Launcher      launcher.Launcher
+	EventLog      eventlog.EventLog
 }
 
 /*
@@ -96,5 +100,18 @@ func Parse(pf []byte) (*policy, error) {
 		p.Collectors = append(p.Collectors, collector)
 	}
 
+	if len(parse.Launcher) > 0 {
+		if err := json.Unmarshal(parse.Launcher, &p.Launcher); err != nil {
+			log.Printf("parse policy: Launcher Unmarshall error=%v!!\n", err)
+			return nil, err
+		}
+	}
+
+	if len(parse.EventLog) > 0 {
+		if err := json.Unmarshal(parse.EventLog, &p.EventLog); err != nil {
+			log.Printf("parse policy: EventLog Unmarshall error=%v!!", err)
+			return nil, err
+		}
+	}
 	return p, nil
 }
