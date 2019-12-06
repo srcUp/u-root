536c6ef48f249ac6f4c49c7e71915c45cc713288
Simran Singh <simran.p.singh@oracle.com>
2019-12-06 15:37:46 -0800

    new securelaunch pkg.
    Signed-off-by: Simran Singh <simran.p.singh@oracle.com>

---
+++ pkg/securelaunch/tpm/tpm.go
@@ -0,0 +1,22 @@
+// Package tpm reads and extends pcrs with measurements.
+package tpm
+
+import (
+	"fmt"
+	"io"
+
+	"github.com/google/go-tpm/tpm2"
+)
+
+/*
+ * GetHandle returns a tpm device handle from go-tpm/tpm2
+ * that can be used for storing hashes.
+ */
+func GetHandle() (io.ReadWriteCloser, error) {
+	tpm2, err := tpm2.OpenTPM("/dev/tpm0")
+	if err != nil {
+		return nil, fmt.Errorf("couldn't talk to TPM Device: err=%v", err)
+	}
+
+	return tpm2, nil
+}
