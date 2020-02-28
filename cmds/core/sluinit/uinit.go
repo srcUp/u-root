4f0f5cd7ce94b69e034cbed49772e1473a01da86
Simran Singh <simran.p.singh@oracle.com>
2020-02-28 12:26:36 -0800

    support to scan iscsi drives in sluinit

--- cmds/core/sluinit/uinit.go
+++ cmds/core/sluinit/uinit.go
@@ -5,11 +5,17 @@
 package main
 
 import (
+	"bytes"
+	"errors"
 	"flag"
+	"fmt"
 	"log"
 	"os"
+	"os/exec"
+	"strings"
 	"time"
 
+	"github.com/u-root/u-root/pkg/cmdline"
 	slaunch "github.com/u-root/u-root/pkg/securelaunch"
 	"github.com/u-root/u-root/pkg/securelaunch/policy"
 	"github.com/u-root/u-root/pkg/securelaunch/tpm"
@@ -131,4 +137,82 @@ func unmountAndExit() {
 	slaunch.UnmountAll()
 	time.Sleep(5 * time.Second) // let queued up debug statements get printed
 	os.Exit(1)
+}
+
+// To run the daemon in debug mode please pass the parameter  '-d <debug level>'
+// DEBUG         4 - Print all messages
+// INFO          3 - Print messages needed to follow the uIP code (default)
+// WARN          2 - Print warning messages
+// ERROR         1 - Only print critical errors
+// netroot=iscsi:@10.196.210.62::3260::iqn.1986-03.com.sun:ovs112-boot rd.iscsi.initiator=iqn.1988-12.com.oracle:ovs112
+// netroot=iscsi:@10.196.210.64::3260::iqn.1986-03.com.sun:ovs112-boot
+//NOTE:  if you have two netroot params in kernel command line , second one will be used.
+func scanIscsiDrives() error {
+
+	log.Printf("Scanning kernel cmd line for *netroot* flag")
+	val, ok := cmdline.Flag("netroot")
+	if !ok {
+		log.Printf("sl_policy netroot flag is not set")
+		return errors.New("Flag Not Set")
+	}
+
+	// val = iscsi:@10.196.210.62::3260::iqn.1986-03.com.sun:ovs112-boot
+	log.Printf("netroot flag is set with val=%s", val)
+	s := strings.Split(val, "::")
+	if len(s) != 3 {
+		return fmt.Errorf("%v: incorrect format ::,  Usage: netroot=iscsi:@10.X.Y.Z::1224::iqn.foo:hostname-bar, [Expecting len(%s) = 3] ", val, s)
+	}
+
+	// s[0] = iscsi:@10.196.210.62 or iscsi:@10.196.210.62,2
+	// s[1] = 3260
+	// s[2] = iqn.1986-03.com.sun:ovs112-boot
+	port := s[1]
+	target := s[2]
+	tmp := strings.Split(s[0], ":@")
+	if len(tmp) > 3 || len(tmp) < 2 {
+		return fmt.Errorf("%v: incorrect format :@, Usage: netroot=iscsi:@10.X.Y.Z::1224::iqn.foo:hostname-bar, [ Expecting 2 <= len(%s) <= 3", val, tmp)
+	}
+
+	if tmp[0] != "iscsi" {
+		return fmt.Errorf("%v: incorrect format iscsi:, Usage: netroot=iscsi:@10.X.Y.Z::1224::iqn.foo:hostname-bar, [ %s != 'iscsi'] ", val, tmp[0])
+	}
+
+	var ip, group string
+	tmp2 := strings.Split(tmp[1], ",")
+	if len(tmp2) == 1 {
+		ip = tmp[1]
+	} else if len(tmp2) == 2 {
+		ip = tmp[1]
+		group = tmp[2]
+	}
+
+	log.Printf("Scanning kernel cmd line for *rd.iscsi.initiator* flag")
+	initiatorName, ok := cmdline.Flag("rd.iscsi.initiator")
+	if !ok {
+		log.Printf("sl_policy rd.iscsi.initiator flag is not set")
+		return errors.New("Flag Not Set")
+	}
+
+	var portalGroup string
+	if group == "" {
+		portalGroup = "1"
+	}
+
+	cmd00 := exec.Command("iscsistart", "-d=ERROR", "-a", ip, "-g", portalGroup, "-p", port, "-t", target, "-i", initiatorName)
+	var out00 bytes.Buffer
+	cmd00.Stdout = &out00
+	log.Printf("Executing %v", cmd00.Args)
+	if err00 := cmd00.Run(); err00 != nil {
+		fmt.Println(err00)
+	} else {
+		log.Printf("Output: %v", cmd00.Stdout)
+	}
+	return nil
+}
+
+func init() {
+	err := scanIscsiDrives()
+	if err != nil {
+		log.Printf("NO ISCSI DRIVES found, err=[%v]", err)
+	}
 }
